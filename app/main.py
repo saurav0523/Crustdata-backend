6a7ae200e657c617b1ab5ee1dd94d97112feb0f4
Saurav Gupta <gsaurav641@gmail.com>
2026-06-02 01:10:50 +0530

    Update CORS to allow all origins

--- app/main.py
+++ app/main.py
@@ -26,8 +26,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],   # Vite dev server
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
